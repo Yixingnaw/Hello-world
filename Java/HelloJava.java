df146e997567b2f5f73e617ab5385289902b9d80
zuoyi <lian89093@gmail.com>
2021-05-23 18:02:52 +0800

    all test

---
+++ Java/HelloJava.java
@@ -0,0 +1,11 @@
+class HelloJava
+{ 
+ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello world from Java!"); 
+     
+        for (int i = 0; i <= 5; i++) 
+            System.out.println("Excellent tutorial on git and github!!! Just loved the article!!!!!!"
+	//Pull request
+} 
---
+++ Java/HelloWorldJava.java
@@ -0,0 +1,11 @@
+class HelloWorldJava
+{ 
+ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello world from Java!"); 
+     
+        for (int i = 0; i <= 5; i++) 
+            System.out.println("Thank you, the author");
+	//Pull request
+} 
